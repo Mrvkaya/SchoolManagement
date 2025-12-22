b4a99e465d52f379b218dc9b614a48f1f36c8448
Merve <mrvky1689@gmail.com>
2025-12-22 22:51:39 +0300

    ....

---
+++ SMIS.UI/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SMIS.DAL.Context;
+using SMIS.Entities.Enums;
+
+namespace SMIS.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SchoolManagementDbContext _context;
+
+        public AccountController(SchoolManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var user = _context.Users
+                .FirstOrDefault(x => x.Username == username && x.Password == password);
+
+            if (user == null)
+            {
+                return Content("Kullanıcı bulunamadı");
+            }
+
+            return user.Role switch
+            {
+                UserRole.Admin => RedirectToAction("Index", "Admin"),
+                UserRole.Teacher => RedirectToAction("Index", "Teacher"),
+                UserRole.Student => RedirectToAction("Index", "Student"),
+                _ => Content("Geçersiz rol")
+            };
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login", "Account");
+        }
+
+    }
+}
---
+++ SMIS.UI/Controllers/TeacherController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMIS.UI.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
